a7be78eb59423b2f47128496d71ab70415cdaa76
Leesing <biggerbing@163.com>
2021-07-16 17:24:03 +0800

    3

---
+++ src/main/java/leesing/Sample.java
@@ -0,0 +1,16 @@
+package leesing;
+
+/**
+ * 描述
+ *
+ * @author Leesing
+ * @date 2021-07-16 星期五 下午 5:23
+ */
+public class Sample {
+
+    private String a;
+
+    private String b;
+
+    private String c;
+}
