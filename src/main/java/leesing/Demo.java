1094b3285ca5412a8df7c717e7d127e764efc6c1
Leesing <biggerbing@163.com>
2021-07-16 17:23:23 +0800

    2

---
+++ src/main/java/leesing/Demo.java
@@ -0,0 +1,18 @@
+package leesing;
+
+/**
+ * 描述
+ *
+ * @author Leesing
+ * @date 2021-07-16 星期五 下午 5:21
+ */
+public class Demo {
+
+    private String name;
+
+    private Integer age;
+
+    private String addr;
+
+    private String city;
+}
